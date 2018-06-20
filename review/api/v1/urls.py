31dc6f05ef608ee58daa8d661b6faf62b612aedb
Murilo Camargos <murilo@ckl.io>
2018-06-20 13:28:32 -0300

    Expose review api endpoints

--- review/api/v1/urls.py
+++ review/api/v1/urls.py
@@ -1,14 +1,19 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework_nested import routers
 
-from review.api.v1.views import CompanyViewSet
+from review.api.v1.views import CompanyViewSet, ReviewViewSet
 
 
 router = routers.SimpleRouter()
-router.register(r'company', CompanyViewSet, base_name='companies')
+router.register(r'company', CompanyViewSet, base_name='company')
+router.register(r'review', ReviewViewSet, base_name='review')
+
+reviews_router = routers.NestedSimpleRouter(router, r'company', lookup='company')
+reviews_router.register(r'review', ReviewViewSet, base_name='company-review')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(reviews_router.urls)),
 ]
--- review/api/v1/views.py
+++ review/api/v1/views.py
@@ -1,13 +1,54 @@
-import re
-
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from review.api.v1.serializers import CompanySerializer
-from review.models import Company
+from review.api.v1.serializers import (CompanySerializer, ReviewCompanySerializer,
+                                       ReviewSerializer)
+from review.models import Company, Review
 
 
 class CompanyViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
     permission_classes = [IsAuthenticated]
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        qs = Review.objects.filter(reviewer=self.request.user)
+
+        if self.kwargs.get('company_pk'):
+            company = get_object_or_404(Company, pk=self.kwargs.get('company_pk'))
+            qs = qs.filter(company=company)
+        
+        return qs.all()
+    
+    def get_serializer_class(self):
+        if self.kwargs.get('company_pk'):
+            return ReviewCompanySerializer
+        return ReviewSerializer
+
+    @staticmethod
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+    
+    def perform_create(self, serializer):
+        if self.kwargs.get('company_pk'):
+            company = get_object_or_404(Company, pk=self.kwargs.get('company_pk'))
+            serializer.save(
+                reviewer=self.request.user,
+                company=company,
+                ip_address=self.get_client_ip(self.request),
+            )
+        else:
+            serializer.save(
+                reviewer=self.request.user,
+                ip_address=self.get_client_ip(self.request),
+            )
