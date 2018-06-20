53ef5c73cd382086de0fc27deaab5742f218a792
Murilo Camargos <murilo@ckl.io>
2018-06-20 11:36:36 -0300

    Remove some tests

--- common/tests.py
+++ common/tests.py
@@ -18,28 +18,3 @@ class CommonTestCase(APITestCase):
 
         self.assertEqual(token.count(), 1)
         self.assertEqual(token.first().user, user)
-
-    def test_api_auth_with_query_string(self):
-        """
-        Tests api authentication using the query parameter `token`.
-        """
-        token = Token.objects.filter(user=self.user).first()
-
-        url = reverse('subscribers-list')
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-        url = reverse('subscribers-list') + '?token=' + token.key
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_api_auth_with_bearer_token(self):
-        """
-        Tests api authentication using token sent as http auth.
-        """
-        token = Token.objects.filter(user=self.user).first()
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-
-        url = reverse('subscribers-list')
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
