69f882e3b2347f74dbb916fdb9c2e830cdfa2f55
Murilo Camargos <murilo@ckl.io>
2018-06-20 12:29:23 -0300

    Register review models to admin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from review.models import Company, Review
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    readonly_fields = ('title', 'company', 'reviewer', 'ip_address', 'rating',\
+        'created_at', 'summary',)
+    list_display = ('title', 'reviewer', 'company', 'rating', 'created_at')
+
+admin.site.register(Company)
+admin.site.register(Review, ReviewAdmin)
